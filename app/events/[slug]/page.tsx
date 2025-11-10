fc3d0fcfcf27f79faada4e41fd953ebf387ecb0b
lhouarhou <l.ouarhou.ensa@gmail.com>
2025-11-10 22:35:18 +0100

    add event details

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,27 +1,19 @@
-import { notFound } from 'next/navigation';
-
-const BASE_URL = process.env.NEXT_BASE_URL;
+import { Suspense } from 'react';
+import EventDetails from '@/components/EventDetails';
 
 const EventDetailsPage = async ({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) => {
-  const { slug } = await params;
-
-  const request = await fetch(`${BASE_URL}/events/${slug}`);
-
-  const { event } = await request.json();
-
-  if (!event) return notFound();
+  const slug = params.then((p) => p.slug);
 
   return (
-    <section id='event'>
-      <h1>
-        Event Details: <br /> {slug}
-      </h1>
-    </section>
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails params={slug} />
+      </Suspense>
+    </main>
   );
 };
-
 export default EventDetailsPage;
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
   title: string;
@@ -20,10 +20,12 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
         height={300}
         className='poster'
       />
+
       <div className='flex flex-row gap-2'>
         <Image src='/icons/pin.svg' alt='location' width={14} height={14} />
         <p>{location}</p>
       </div>
+
       <p className='title'>{title}</p>
 
       <div className='datetime'>
@@ -31,7 +33,6 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
           <Image src='/icons/calendar.svg' alt='date' width={14} height={14} />
           <p>{date}</p>
         </div>
-
         <div>
           <Image src='/icons/clock.svg' alt='time' width={14} height={14} />
           <p>{time}</p>
